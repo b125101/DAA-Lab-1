e195e0bd7a76a36968cfa8d8f3641e3d12fd33a2
b125101 <b125101@iiit-bh.ac.in>
2026-08-25 11:21:32 +0530

    Update q6_max_overlap_point.c

--- Lab 4/q6_max_overlap_point.c
+++ Lab 4/q6_max_overlap_point.c
@@ -1,5 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct{int x,delta;} Event;
-int cmp(const void*a,const void*b){const Event*x=a,*y=b;if(x->x!=y->x)return x->x-y->x;return y->delta-x->delta;}
-int main(){int n;scanf("%d",&n);Event*e=malloc(2*n*sizeof(Event));for(int i=0;i<n;i++){int l,r;scanf("%d %d",&l,&r);e[2*i]=(Event){l,1};e[2*i+1]=(Event){r,-1};}qsort(e,2*n,sizeof(Event),cmp);int cur=0,mx=0,p=0;for(int i=0;i<2*n;i++){cur+=e[i].delta;if(cur>mx){mx=cur;p=e[i].x;}}printf("Point: %d\nMaximum overlapping intervals: %d\n",p,mx);free(e);return 0;}
+
+struct Event {
+    int point;
+    int type;
+};
+
+int compare(const void *a, const void *b) {
+    struct Event *e1 = (struct Event *)a;
+    struct Event *e2 = (struct Event *)b;
+
+    if (e1->point != e2->point)
+        return e1->point - e2->point;
+
+    // Start event before end event
+    if (e1->type == 1 && e2->type == -1)
+        return -1;
+
+    if (e1->type == -1 && e2->type == 1)
+        return 1;
+
+    return 0;
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of intervals: ");
+    scanf("%d", &n);
+
+    struct Event events[2 * n];
+
+    printf("Enter intervals:\n");
+
+    for (int i = 0; i < n; i++) {
+        int left, right;
+
+        printf("Interval %d: ", i + 1);
+        scanf("%d %d", &left, &right);
+
+        events[2 * i].point = left;
+        events[2 * i].type = 1;      // Start
+
+        events[2 * i + 1].point = right;
+        events[2 * i + 1].type = -1; // End
+    }
+
+    // Sort events
+    qsort(events, 2 * n, sizeof(struct Event), compare);
+
+    int current = 0;
+    int maximum = 0;
+    int bestPoint = 0;
+
+    for (int i = 0; i < 2 * n; i++) {
+
+        current += events[i].type;
+
+        if (current > maximum) {
+            maximum = current;
+            bestPoint = events[i].point;
+        }
+    }
+
+    printf("\nPoint with maximum overlap = %d\n", bestPoint);
+    printf("Maximum number of intervals = %d\n", maximum);
+
+    return 0;
+}
